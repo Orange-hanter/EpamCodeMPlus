3b8a5cde1d8f8f82006992981133bc15f3daab8f
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2022-09-19 22:23:21 +0300

    Applying _rule of five_

--- Clone/Clone.App/Package.hpp
+++ Clone/Clone.App/Package.hpp
@@ -17,14 +17,16 @@ struct Frame {
     _frameSize = other._frameSize;
   }
 
+  Frame(const Frame& other)
+  {
+    _frameSize = other._frameSize;
+    memcpy(_buf, other._buf, _frameSize);
+  }
+
   ~Frame()
   {
     if (_buf != nullptr) delete[] _buf;
   }
-
-  char* data() const { return _buf; }
-
-  size_t frameSize() const { return _frameSize; };
 
   Frame& operator=(Frame&& other)
   {
@@ -34,12 +36,23 @@ struct Frame {
     return *this;
   }
 
+  Frame& operator=(const Frame& other)
+  {
+    _frameSize = other._frameSize;
+    memcpy(_buf, other._buf, _frameSize);
+    return *this;
+  }
+
   friend std::ifstream& operator>>(std::ifstream& ifs, Frame& frame)
   {
     ifs.read(frame._buf, frame._frameSize);
     frame._frameSize = static_cast<size_t>(ifs.gcount());
     return ifs;
   }
+
+  char* data() const { return _buf; }
+
+  size_t frameSize() const { return _frameSize; };
 
  private:
   size_t _frameSize;
