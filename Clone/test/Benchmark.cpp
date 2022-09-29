56278c1235ff2aeb3c37730109ce091739c6848f
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2022-09-29 14:05:29 +0300

    Benchmark improvement Now it calculate average time

--- Clone/test/Benchmark.cpp
+++ Clone/test/Benchmark.cpp
@@ -1,47 +1,52 @@
-#include <algorithm>
 #include <chrono>
 #include <filesystem>
 #include <iostream>
+#include <numeric>
+#include <vector>
 
 #include "../Clone.App/Director.hpp"
 
 using namespace std::chrono;
-using namespace std::filesystem;
+using std::filesystem::path;
 
 static const uint32_t KByte_s = 1024;
 static const uint32_t MByte_s = KByte_s * 1024;
-static const uint32_t testingMemorySize = 100;
-static char mem[MByte_s];
+static const uint32_t testingMemoryBlockCount = 100;
+static const uint32_t testingMemoryBlockSize = MByte_s;
 
 void generateFile(path sourceFile)
 {
+  static char mem[testingMemoryBlockSize];
+  std::fill_n(mem, testingMemoryBlockSize, 0x55);
   std::ofstream file(sourceFile);
-  for (size_t i = 0; i < testingMemorySize; i++) file << mem;
-  // file << '\0';
+  for (size_t i = 0; i < testingMemoryBlockCount; i++) file << mem;
   file.close();
 }
 
 int main(int argc, char** arv)
 {
-  std::fill_n(mem, MByte_s, 0x55);
   path sourceFile = "src.data";
   path destinationFile = "dst.data";
 
   generateFile(sourceFile);
 
-  auto start = steady_clock::now();
-  {
-    Clone::Director director(absolute(sourceFile).string(),
-                             absolute(sourceFile).string());
-    director.work();
+  std::vector<int> time;
+
+  for (int i = 0; i < 10; i++) {
+    auto start = steady_clock::now();
+    {
+      Clone::Director director(absolute(sourceFile).string(),
+                               absolute(destinationFile).string());
+      director.work();
+    }
+    auto end = steady_clock::now();
+    time.push_back(duration_cast<milliseconds>(end - start).count());
+    std::cout << "Attempt " << i << ' ' << time[i] << " ms\n";
+    remove(destinationFile);
   }
-  auto end = steady_clock::now();
-
   remove(sourceFile);
-  remove(destinationFile);
-
-  std::cout << "Elapsed time in microseconds: "
-            << duration_cast<microseconds>(end - start).count() << " ms"
-            << std::endl;
+  std::cout << "\nAverage time of copying is: "
+            << std::accumulate(time.begin(), time.end(), 0) / time.size()
+            << "ms";
   return 0;
 }
