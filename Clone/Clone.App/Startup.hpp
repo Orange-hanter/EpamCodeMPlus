1a844b54c36a7978d4e061ffdd8e7a1adfe147cb
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-05-04 18:13:34 +0300

    Change workflow of app startup

--- Clone/Clone.App/Startup.hpp
+++ Clone/Clone.App/Startup.hpp
@@ -10,6 +10,7 @@ class StartupArgumentsParser {
   StartupArgumentsParser(int argc, char** argv)
   {
     _parser = std::make_unique<argparse::ArgumentParser>("Copy");
+    _config = std::make_unique<Configuration>();
 
     _parser->add_description(
         "Copy tool app implement simple 2 threaded \"copy\" tool."
@@ -22,37 +23,30 @@ class StartupArgumentsParser {
     _parser->add_argument("-s", "--source")
         .required()
         .default_value<std::string>("-")
-        .help("Absolute path to original file.")
-        .action([this](const std::string& value) { config->set_param("source", value); });
+        .help("Absolute path to original file.");
 
-    _parser->add_argument("-o", "--destination")
+    _parser->add_argument("-d", "--destination")
         .default_value<std::string>("-")
-        .help("Absolute path to the new file.")
-        .action([this](const std::string& value) { config->set_param("destination", value); });
+        .help("Absolute path to the new file.");
 
     _parser->add_argument("-f", "--frame")
         .help("Max size of one package.")
-        .default_value<uint16_t>(32)
+        .default_value<std::string>("32")
         .action([this](const std::string& value) {
-          config->set_param("frame", value);
+          _config->set_param("frame", value);
         });
 
-    _parser->add_argument("--ipc")
+    _parser->add_argument("--ipc", "--mode")
         .help("Turn on interprocess communication mode.")
-        .default_value(false)
-        .implicit_value(true)
-        .action([this](const std::string& value) { config->set_param("mode", value);
-        });
+        .default_value<std::string>("BITESTREAM")
+        .implicit_value(std::string("IPC"));
 
-    _parser->add_argument("--role_host")
+    _parser->add_argument("--client", "--role")
         .help(
             "Work only with --ipc mode. Take role of host, and fill shared "
             "memory by data. By default used host mode")
-        .default_value(true)
-        .implicit_value(true)
-        .action(
-            [this](const std::string& value) { config->set_param("role", value); });
-
+        .default_value<std::string>("HOST")
+        .implicit_value(std::string("CLIENT"));
 
     try {
       _parser->parse_args(argc, argv);
@@ -72,13 +66,24 @@ class StartupArgumentsParser {
 
   std::shared_ptr<Configuration> getConfiguration()
   {
-    return config;
+    auto set = [this](const std::string & param){
+      auto key = std::string("--") + param;
+      auto value = _parser->get(key);
+      _config->set_param(param, value);
+    };
+    set(CfgProperties::source);
+    set(CfgProperties::destination);
+    set(CfgProperties::frame);
+    set(CfgProperties::role);
+    set(CfgProperties::mode);
+
+    return _config;
   }
 
   bool isFlag(std::string_view flag) const { return _parser->is_used(flag); }
 
  private:
   std::unique_ptr<argparse::ArgumentParser> _parser;
-  std::shared_ptr<Configuration> config;
+  std::shared_ptr<Configuration> _config;
 };
 }  // namespace Clone::Parser
--- Clone/Core/Common/Configuration.hpp
+++ Clone/Core/Common/Configuration.hpp
@@ -1,54 +1,71 @@
 #pragma once
 #include <exception>
-#include <string>
 #include <map>
+#include <string>
 
-/*
-we have next fields
-- 
-- 
-- 
-- 
-- 
-*/
 namespace Clone::Parser {
-    
-    struct CfgProperties {
-        static inline const char * source = "source";
-        static inline const char * destination = "destination";
-        static inline const char * frame = "frame";
-        static inline const char * mode = "mode";
-        static inline const char * role = "role";
-    };
 
-    class Configuration
+enum class CopyingMode {
+  BitStream,           // byte per byte copying in multithread way
+  SharedMemoryStream,  // per data chunks shared via unified memory
+};
+
+struct CfgProperties {
+  static inline const char *source = "source";
+  static inline const char *destination = "destination";
+  static inline const char *frame = "frame";
+  static inline const char *mode = "ipc";
+  static inline const char *role = "role";
+};
+
+class Configuration {
+ public:
+  explicit Configuration() = default;
+  Configuration(const Configuration &) = default;
+  ~Configuration() = default;
+
+  void set_param(std::string key, std::string value)
+  {
+    if (_properties.find(key) == _properties.end())
+      _properties.emplace(std::move(key), std::move(value));
+    else
+      throw std::exception("Parameter duplication");
+  }
+
+  template <typename return_t>
+  return_t get_param(const std::string & key)
+  {
+    throw std::bad_cast();
+  }
+
+  template <>
+  std::string get_param<std::string>(const std::string & key)
+  {
+    if (auto search = _properties.find(key); search != _properties.end())
+      return search->second;
+    throw std::exception();
+  }
+
+  template <>
+  CopyingMode get_param<CopyingMode>(const std::string & key)
+  {
+    // todo need more checks
+    if (auto search = _properties.find(key); search != _properties.end())
     {
-        
-    public:
-        explicit Configuration() = default;
-        Configuration(const Configuration & ) = default;
-        ~Configuration() = default;
+      if (search->second.compare("IPC") == 0) return CopyingMode::SharedMemoryStream;
+      if (search->second.compare("BITESTREAM") == 0) return CopyingMode::BitStream;
+    }
+    throw std::exception("Bad key: ");
+  }
 
-        void set_param(std::string key, std::string value)
-        {
-            _properties.emplace(key, value);
-        }
+  void print()
+  {
+    for (const auto &p : _properties) {
+      std::cout << p.first << " => " << p.second << '\n';
+    }
+  }
 
-        template<typename return_t>
-        return_t get_param(std::string key){
-                if(auto search = _properties.find(key); search != _properties.end())
-                    return static_cast<return_t>(search->second);
-                throw std::exception();
-        }
-
-        std::string get_param(std::string key)
-        {
-            if(auto search = _properties.find(key); search != _properties.end())
-                    return search->second;
-                throw std::exception();
-        }
-
-        private:
-        std::map<std::string, std::string> _properties;
-    };
-}
+ private:
+  std::map<std::string, std::string> _properties;
+};
+}  // namespace Clone::Parser
