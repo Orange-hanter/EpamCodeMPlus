1555d45ff5dbfe23f753a5a535015a8e5d9f8b69
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-06-02 10:36:15 +0300

    Sync

--- Src/Core/TransportLayer/Downloader.hpp
+++ Src/Core/TransportLayer/Downloader.hpp
@@ -6,22 +6,42 @@
 #define EPAMMIDDLE_DOWNLOADER_HPP
 
 #include <memory>
+#include <filesystem>
+
+using fs = std::filesystem;
 
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
+
+#include "messages.pb.h"
 
 using asio::ip::tcp;
 
 namespace Clone::TransportLayer {
 
+
 class Downloader : public std::enable_shared_from_this<Downloader> {
+
+  void do_read_header()
+  {
+   
+  }
+
  public:
-  Downloader(asio::io_context ctx): m_context(ctx)
+  explicit Downloader(asio::io_context ctx): m_context(ctx), m_socket(ctx)
   {}
+
+  void start()
+  {
+   do_read_header();
+  }
+
+  tcp::socket& socket() { return socket_; }
 
   private:
    asio::io_context& m_context;
-   tcp::socket socket_;
+   tcp::socket m_socket;
+   fs::path m_tmp_file;
 };
 
 } /// End Clone::TransportLayer
--- Src/Core/TransportLayer/TCPTransportLayer.hpp
+++ Src/Core/TransportLayer/TCPTransportLayer.hpp
@@ -7,6 +7,8 @@
 
 #include <asio.hpp>
 
+#include <set>
+
 #include "iTransportLayer.hpp"
 #include "Downloader.hpp"
 
@@ -23,16 +25,16 @@ class TCPAcceptor {
   void start_accept()
   {
     auto newSession = std::make_shared<Downloader>(_ioContext);
-
+    _DLManager.insert(newSession);
+    using error = std::placeholders::_1;
     _acceptor->async_accept(newSession->socket(),
-        std::bind(&TCPAcceptor::handle_accept, this,
-                                      newSession,
-                  std::placeholders::_1));
+        std::bind(&TCPAcceptor::handle_accept, this, error, newSession)
+        );
   }
 
-  void handle_accept(const Downloader& new_connection,
-                     const asio::error_code& error)
+  void handle_accept(const asio::error_code& error, const Downloader& new_connection,)
   {
+    // TODO: not shure that extra call won't harm performance
     if (!error) {
       new_connection->start();
     }
@@ -51,6 +53,7 @@ class TCPAcceptor {
  private:
   asio::io_context _ioContext;
   std::unique_ptr<tcp::acceptor> _acceptor;
+  std::set<std::shared_ptr<Downloader>> _DLManager;
 };
 
 }  // namespace Clone::TransportLayer
